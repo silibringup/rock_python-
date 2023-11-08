a31b54de39c76c5763772b447ef473d08f8c039a
Jagger Yang <jaggery@pdx1-sim-m42-023.nvidia.com>
2023-11-07 17:33:45 -0800

    update the fabric plm trest

--- tests/erot_fab_plm_test.py
+++ tests/erot_fab_plm_test.py
@@ -230,6 +230,7 @@ with Test(sys.argv) as t:
                         if(other_fuse['fuse_name'] != fuse['fuse_name']):
                             helper.log("checking the other fuse %s, and the reg is %s" % (other_fuse['fuse_name'], other_fuse['reg']))
                             check_fuse0(other_fuse)
+                            helper.log("checking the other fuse %s Done" % (other_fuse['fuse_name'], other_fuse['reg']))
                             break
                 else:
                     for other_fuse in FULL_PLM_LIST:
