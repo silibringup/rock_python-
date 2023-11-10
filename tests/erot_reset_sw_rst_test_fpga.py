1c10855ae11b98c70ef2e558a3da8dd0c9b2dadf
Lizhou Wang <lizhouw@pdx-xterm-121.nvidia.com>
2023-11-09 19:22:17 -0800

    rst_test

--- tests/erot_reset_sw_rst_test_fpga.py
+++ tests/erot_reset_sw_rst_test_fpga.py
@@ -52,6 +52,7 @@ with Test(sys.argv) as t:
                 exp = l0_reg.reset_val & mask
             if act != exp:
                 helper.perror("Mismatch, %s's value is not as expected" % l0_reg.name)
+            helper.log(f'reg name: {l0_reg.name}, act: {hex(act)}, exp: {hex(exp)}')
         helper.log("L0 reset domain reg check done")
 
     def l1_rst_domain_reg_check(after_reset):
@@ -67,6 +68,7 @@ with Test(sys.argv) as t:
                 exp = l1_reg.reset_val & mask
             if act != exp:
                 helper.perror("Mismatch, %s's value is not as expected" % l1_reg.name)
+            helper.log(f'reg name: {l1_reg.name}, act: {hex(act)}, exp: {hex(exp)}')
         helper.log("L1 reset domain reg check done")
 
     def l3_rst_domain_reg_check(after_reset):
@@ -82,6 +84,7 @@ with Test(sys.argv) as t:
                 exp = l3_reg.reset_val & mask
             if act != exp:
                 helper.perror("Mismatch, %s's value is not as expected" % l3_reg.name)
+            helper.log(f'reg name: {l3_reg.name}, act: {hex(act)}, exp: {hex(exp)}')
         helper.log("L3 reset domain reg check done")
 
 #    def chk_reg_rst_value(reg):
@@ -108,10 +111,9 @@ with Test(sys.argv) as t:
             act = rd.value & mask
             count += 1
             if act == exp:
-                helper.log(f"Poll REG {reg.name} done after {count} times. Reg value = {hex(act)}")
+                helper.log(f"SW RST check: Poll REG {reg.name} done after {count} times. Reg value = {hex(act)}")
                 return
-        helper.perror(f"Poll timeout after {count} times try. Reg value = {hex(act)}. Exp value = {hex(exp)}")
-
+        helper.perror(f"SW RST check: Poll timeout after {count} times try. Reg value = {hex(act)}. Exp value = {hex(exp)}")
 
     def deassert_sw_reset_l1():
         erot.RESET.NVEROT_RESET_CFG.SW_GPIO_CTRL_RST_0.write(RESET_GPIO_CTRL=1)
@@ -142,74 +144,110 @@ with Test(sys.argv) as t:
     deassert_sw_reset_l3()
 
     # check L1 domain SW rst
-    helper.log("Configure L1 SW reset one-by-one")
+    helper.log("###############################################################################")
+    helper.log("###################### Configure L1 SW reset one-by-one #######################")
+    helper.log("###############################################################################")
 
+    helper.log("###################### Check GPIO #######################")
     reg_cfg()
     helper.wait_sim_time("us", 5)
-    helper.log("Check GPIO")
+    l0_rst_domain_reg_check(after_reset=0)
+    l1_rst_domain_reg_check(after_reset=0)
+    l3_rst_domain_reg_check(after_reset=0)
+    helper.wait_sim_time("us", 5)
     erot.RESET.NVEROT_RESET_CFG.SW_GPIO_CTRL_RST_0.write(RESET_GPIO_CTRL=0)
     erot.RESET.NVEROT_RESET_CFG.SW_GPIO_CTRL_RST_0.write(RESET_GPIO_CTRL=1)
     chk_reg_rst_value(erot.GPIO.A_VM_01_0)
-    helper.log("GPIO done")
+    helper.log("###################### GPIO done #######################")
 
+    helper.log("###################### Check BYPASS MON0 ######################")
     reg_cfg()
     helper.wait_sim_time("us", 5)
-    helper.log("Check BYPASS MON0")
+    l0_rst_domain_reg_check(after_reset=0)
+    l1_rst_domain_reg_check(after_reset=0)
+    l3_rst_domain_reg_check(after_reset=0)
+    helper.wait_sim_time("us", 5)
     erot.RESET.NVEROT_RESET_CFG.SW_SPIMON0_RST_0.write(RESET_SPIMON0=0)
     erot.RESET.NVEROT_RESET_CFG.SW_SPIMON0_RST_0.write(RESET_SPIMON0=1)
     chk_reg_rst_value(erot.SPI_MON0.reg7_end_address_0)
-    helper.log("BYPASS MON0 done")
+    helper.log("###################### BYPASS MON0 done ######################")
 
+    helper.log("###################### Check BYPASS MON1 ######################")
     reg_cfg()
     helper.wait_sim_time("us", 5)
-    helper.log("Check BYPASS MON1")
+    l0_rst_domain_reg_check(after_reset=0)
+    l1_rst_domain_reg_check(after_reset=0)
+    l3_rst_domain_reg_check(after_reset=0)
+    helper.wait_sim_time("us", 5)
     erot.RESET.NVEROT_RESET_CFG.SW_SPIMON1_RST_0.write(RESET_SPIMON1=0)
     erot.RESET.NVEROT_RESET_CFG.SW_SPIMON1_RST_0.write(RESET_SPIMON1=1)
     chk_reg_rst_value(erot.SPI_MON1.reg7_end_address_0)
-    helper.log("BYPASS MON1 done")
+    helper.log("###################### BYPASS MON1 done ######################")
 
     # check L3 domain SW rst
-    helper.log("Configure L3 SW reset one-by-one")
+    helper.log("###############################################################################")
+    helper.log("###################### Configure L3 SW reset one-by-one #######################")
+    helper.log("###############################################################################")
 
+    helper.log("###################### Check ioexpander I2C ######################")
     reg_cfg()
     helper.wait_sim_time("us", 5)
-    helper.log("Check ioexpander I2C")
+    l0_rst_domain_reg_check(after_reset=0)
+    l1_rst_domain_reg_check(after_reset=0)
+    l3_rst_domain_reg_check(after_reset=0)
+    helper.wait_sim_time("us", 5)
     erot.RESET.NVEROT_RESET_CFG.SW_IO_EXP_RST_0.write(RESET_IO_EXP=0)
     erot.RESET.NVEROT_RESET_CFG.SW_IO_EXP_RST_0.write(RESET_IO_EXP=1)
     chk_reg_rst_value(erot.IO_EXPANDER.I2C_CMD_DATA2_0)
-    helper.log("ioexpander I2C done")
+    helper.log("###################### ioexpander I2C done ######################")
 
+    helper.log("###################### Check OOB SPI ######################")
     reg_cfg()
     helper.wait_sim_time("us", 5)
-    helper.log("Check OOB SPI")
+    l0_rst_domain_reg_check(after_reset=0)
+    l1_rst_domain_reg_check(after_reset=0)
+    l3_rst_domain_reg_check(after_reset=0)
+    helper.wait_sim_time("us", 5)
     erot.RESET.NVEROT_RESET_CFG.SW_OOB_SPI_RST_0.write(RESET_OOB_SPI=0)
     erot.RESET.NVEROT_RESET_CFG.SW_OOB_SPI_RST_0.write(RESET_OOB_SPI=1)
     chk_reg_rst_value(erot.OOBHUB_SPI.COMMAND2_0)
-    helper.log("OOB SPI done")
+    helper.log("###################### OOB SPI done ######################")
 
+    helper.log("###################### Check UART ######################")
     reg_cfg()
     helper.wait_sim_time("us", 5)
-    helper.log("Check UART")
+    l0_rst_domain_reg_check(after_reset=0)
+    l1_rst_domain_reg_check(after_reset=0)
+    l3_rst_domain_reg_check(after_reset=0)
+    helper.wait_sim_time("us", 5)
     erot.RESET.NVEROT_RESET_CFG.SW_UART_RST_0.write(RESET_UART=0)
     erot.RESET.NVEROT_RESET_CFG.SW_UART_RST_0.write(RESET_UART=1)
     chk_reg_rst_value(erot.UART.UARTIBRD_0)
-    helper.log("UART done")
+    helper.log("###################### UART done ######################")
 
+    helper.log("###################### Check IB0 SPI ######################")
     reg_cfg()
     helper.wait_sim_time("us", 5)
-    helper.log("Check IB0 SPI")
+    l0_rst_domain_reg_check(after_reset=0)
+    l1_rst_domain_reg_check(after_reset=0)
+    l3_rst_domain_reg_check(after_reset=0)
+    helper.wait_sim_time("us", 5)
     erot.RESET.NVEROT_RESET_CFG.SW_IB0_SPI_RST_0.write(RESET_IB0_SPI=0)
     erot.RESET.NVEROT_RESET_CFG.SW_IB0_SPI_RST_0.write(RESET_IB0_SPI=1)
     chk_reg_rst_value(erot.SPI_IB0.COMMAND2_0)
-    helper.log("IB0 SPI done")
+    helper.log("###################### IB0 SPI done ######################")
 
+    helper.log("###################### Check IB1 SPI ######################")
     reg_cfg()
     helper.wait_sim_time("us", 5)
-    helper.log("Check IB1 SPI")
+    l0_rst_domain_reg_check(after_reset=0)
+    l1_rst_domain_reg_check(after_reset=0)
+    l3_rst_domain_reg_check(after_reset=0)
+    helper.wait_sim_time("us", 5)
     erot.RESET.NVEROT_RESET_CFG.SW_IB1_SPI_RST_0.write(RESET_IB1_SPI=0)
     erot.RESET.NVEROT_RESET_CFG.SW_IB1_SPI_RST_0.write(RESET_IB1_SPI=1)
     chk_reg_rst_value(erot.SPI_IB1.COMMAND2_0)
-    helper.log("IB1 SPI done")
+    helper.log("###################### IB1 SPI done ######################")
 
     # NOT test IB0_I2C on FPGA, since test case is loaded into FPGA thru IB0_I2C
     #reg_cfg()
@@ -220,13 +258,17 @@ with Test(sys.argv) as t:
     #chk_reg_rst_value(erot.I2C_IB0.I2C_CMD_DATA2_0)
     #helper.log("IB0 I2C done")
 
+    helper.log("###################### Check IB1 I2C ######################")
     reg_cfg()
     helper.wait_sim_time("us", 5)
-    helper.log("Check IB1 I2C")
+    l0_rst_domain_reg_check(after_reset=0)
+    l1_rst_domain_reg_check(after_reset=0)
+    l3_rst_domain_reg_check(after_reset=0)
+    helper.wait_sim_time("us", 5)
     erot.RESET.NVEROT_RESET_CFG.SW_IB1_I2C_RST_0.write(RESET_IB1_I2C=0)
     erot.RESET.NVEROT_RESET_CFG.SW_IB1_I2C_RST_0.write(RESET_IB1_I2C=1)
     chk_reg_rst_value(erot.I2C_IB1.I2C_CMD_DATA2_0)
-    helper.log("IB1 I2C done")
+    helper.log("###################### IB1 I2C done ######################")
 
 
     helper.log("All SW RST done")
