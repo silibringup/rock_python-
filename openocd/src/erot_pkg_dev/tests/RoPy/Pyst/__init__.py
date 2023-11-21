4482626c0d3bbcb17e5b9dc0694c40a829066961
changliu <changliu@nvidia.com>
2023-11-21 11:18:52 +0800

    remove unused dirs

--- openocd/src/erot_pkg_dev/tests/RoPy/Pyst/__init__.py
+++
@@ -1,5 +0,0 @@
-from RoPy.Pyst.py.PystParam import *
-from RoPy.Pyst.py.PystPacket import *
-from RoPy.Pyst.py.PystArgument import *
-from RoPy.Pyst.py.PystClient import *
-from RoPy.Pyst.py.PystLink import *
