651508e615bbcb915464e065c0dde6955a5a22d2
Jagger Yang <jaggery@pdx3-sim-f40-042.nvidia.com>
2023-11-21 23:37:24 -0800

    update the fuse api test

--- tests/erot_fuse_override_api_test.py
+++ tests/erot_fuse_override_api_test.py
@@ -8,6 +8,26 @@ from driver import *
 ###################################################
 with Test(sys.argv) as t:
     helper.log("test start")
-    test_api.fuse_opts_override("opt_secure_pri_source_isolation_en", 1)
+    #jtag unlock
+    helper.wait_sim_time("us", 50)
+    helper.hdl_force('ntb_top.u_nv_fpga_dut.u_nv_top_fpga.u_nv_top_wrapper.u_nv_top.nvjtag_sel', 1)
 
+    helper.jtag.Reset(0)
+    helper.jtag.DRScan(100, hex(0x0)) #add some delay as jtag only work when nvjtag_sel stable in real case
+    helper.jtag.Reset(1)
+
+
+    helper.pinfo(f'j2h_unlock sequence start')
+    helper.j2h_unlock()
+    helper.pinfo(f'j2h_unlock sequence finish')
+
+    helper.jtag.DRScan(100, hex(0x0)) #add some delay as jtag only work when nvjtag_sel stable in real case
+    
+    opt_security_mode_addr = 5468 + erot.FUSE.base
+    opt_data = helper.j2h_read(opt_security_mode_addr)
+    helper.log(f"the opt_security_mode_addr value is {opt_data}")
+
+    test_api.fuse_opts_override("opt_secure_pri_source_isolation_en", 1, debug_mode=1)
+    test_api.fuse_opts_override("opt_en_reserved_1", 10100101101010101010101010101010, debug_mode=1)
+    test_api.fuse_opts_override("opt_fsp_peregrine_reserved", 10100101, debug_mode=1)
     helper.log("test finish")
